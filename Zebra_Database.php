0bdd14b261e75239fbf56345fb11c67cda75593c
Stefan Gabos <contact@stefangabos.ro>
2016-02-19 11:27:46 +0200

    Updated version number to 2.9.3

--- Zebra_Database.php
+++ Zebra_Database.php
@@ -24,7 +24,7 @@
  *  For more resources visit {@link http://stefangabos.ro/}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    2.9.3 (last revision: February 15, 2016)
+ *  @version    2.9.3 (last revision: February 19, 2016)
  *  @copyright  (c) 2006 - 2016 Stefan Gabos
  *  @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_Database
