c613fcb484e2de9e3888a17a171ab11ac6dc454a
Stefan Gabos <contact@stefangabos.ro>
2025-08-21 23:54:59 +0300

    Fixed #90

--- Zebra_Database.php
+++ Zebra_Database.php
@@ -10,7 +10,7 @@
  *  Read more {@link https://github.com/stefangabos/Zebra_Database here}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    2.12.1 (last revision: February 07, 2025)
+ *  @version    2.13.0 (last revision: August 21, 2025)
  *  @copyright  © 2006 - 2025 Stefan Gabos
  *  @license    https://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_Database
