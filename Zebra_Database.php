17b06e87dfb6c47ee88e3840c8baab2c4fd03790
Stefan Gabos <contact@stefangabos.ro>
2025-08-22 18:15:22 +0300

    Fixed bug related to #90

--- Zebra_Database.php
+++ Zebra_Database.php
@@ -10,7 +10,7 @@
  *  Read more {@link https://github.com/stefangabos/Zebra_Database here}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    2.13.0 (last revision: August 21, 2025)
+ *  @version    2.13.0 (last revision: August 22, 2025)
  *  @copyright  © 2006 - 2025 Stefan Gabos
  *  @license    https://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_Database
@@ -4646,6 +4646,11 @@ class Zebra_Database {
                     isset($this->ssl_options['capath']) ? $this->ssl_options['capath'] : null,
                     isset($this->ssl_options['cipher']) ? $this->ssl_options['cipher'] : null
                 );
+
+                // handle SSL verification option if provided
+                if (isset($this->ssl_options['verify'])) {
+                    $this->connection->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, $this->ssl_options['verify']);
+                }
 
             }
 
