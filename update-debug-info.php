d20324b784a7623e957f1d560cf92d6ef0f4f47e
Stefan Gabos <contact@stefangabos.ro>
2025-08-22 09:37:17 +0300

    Fixed bug

--- update-debug-info.php
+++ update-debug-info.php
@@ -14,7 +14,7 @@
  */
 
 // no POST data, only a single GET parameter, is the right GET parameter, it contains a valid ID (numeric only), a file with the right name exists (zdb-log-[numeric ID])
-if (empty($_POST) && isset($_GET) && is_array($_GET) && count($_GET) == 1 && isset($_GET['id']) && preg_match('/^[0-9]{25}$/', $_GET['id']) && file_exists(($path = sys_get_temp_dir() . '/zdb-log-' . $_GET['id'])) && is_file($path)) {
+if (empty($_POST) && isset($_GET) && is_array($_GET) && count($_GET) == 1 && isset($_GET['id']) && preg_match('/^[0-9]{12}$/', $_GET['id']) && file_exists(($path = sys_get_temp_dir() . '/zdb-log-' . $_GET['id'])) && is_file($path)) {
     echo file_get_contents($path);
     unlink($path);
     die();
