2bdc973073d3cfffef9f214bf15770fd7677779d
Stefan Gabos <contact@stefangabos.ro>
2026-07-27 19:18:34 +0300

    Updated documentation

--- Zebra_Database.php
+++ Zebra_Database.php
@@ -3206,10 +3206,9 @@ class Zebra_Database {
      *
      *                                  The caching method is specified through the {@link caching_method} property.
      *
-     *                                  *Only queries that return a result set are cached. Besides `SELECT` this also
-     *                                  covers statements like `SHOW`, `DESCRIBE` and `EXPLAIN`. For queries that return
-     *                                  no rows, like `INSERT`, `UPDATE` or `DELETE`, this argument is ignored and the
-     *                                  query is executed every time.*
+     *                                  *Only queries that return a result set are cached (`SELECT`, `SHOW`, `DESCRIBE`
+     *                                  and `EXPLAIN`). For queries like `INSERT`, `UPDATE` or `DELETE`, this argument
+     *                                  is ignored and the query is executed every time.*
      *
      *                                  *For {@link query_unbuffered unbuffered queries} this argument is always `FALSE`!*
      *
